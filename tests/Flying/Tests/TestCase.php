bdaf0fe1e1fc503d51bf51b74b2e268b83ea8654
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2014-01-03 15:10:27 +0400

    Added previously missed Mockery integration code

--- tests/Flying/Tests/TestCase.php
+++ tests/Flying/Tests/TestCase.php
@@ -2,6 +2,16 @@
 
 namespace Flying\Tests;
 
+use Mockery;
+
 class TestCase extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        // Integrate with Mockery
+        Mockery::close();
+    }
 }
